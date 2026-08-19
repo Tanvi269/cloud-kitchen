113264205676f79950d37d575223890ae7c3914c
Tanvi Patil <tanvisp2005@gmail.com>
2026-08-19 19:40:37 +0530

    Add multiple item Selenium test

--- src/test/java/com/cloud/kitchen/CloudKitchenSeleniumTest.java
+++ src/test/java/com/cloud/kitchen/CloudKitchenSeleniumTest.java
@@ -18,20 +18,55 @@ public class CloudKitchenSeleniumTest {
             driver.get("http://localhost:8081/cloud-kitchen/");
             driver.manage().window().maximize();
 
-            // Click Chicken Biryani Add to Cart button
+            // Add Chicken Biryani
             driver.findElement(
                 By.xpath("//button[contains(@onclick,\"Chicken Biryani\")]")
             ).click();
 
-            // Close the JavaScript alert
+            // Close alert
             driver.switchTo().alert().accept();
 
-            // Check cart text
+            // Check cart
             String cartText = driver.findElement(By.id("cart")).getText();
 
             System.out.println("Cart after adding item: " + cartText);
 
             assertTrue(cartText.contains("1"));
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    void testAddMultipleItems() {
+
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("http://localhost:8081/cloud-kitchen/");
+            driver.manage().window().maximize();
+
+            // Add Pizza
+            driver.findElement(
+                By.xpath("//button[contains(@onclick,\"Pizza\")]")
+            ).click();
+
+            driver.switchTo().alert().accept();
+
+            // Add Classic Burger
+            driver.findElement(
+                By.xpath("//button[contains(@onclick,\"Classic Burger\")]")
+            ).click();
+
+            driver.switchTo().alert().accept();
+
+            // Check cart
+            String cartText = driver.findElement(By.id("cart")).getText();
+
+            System.out.println("Cart after adding two items: " + cartText);
+
+            assertTrue(cartText.contains("2"));
 
         } finally {
             driver.quit();
